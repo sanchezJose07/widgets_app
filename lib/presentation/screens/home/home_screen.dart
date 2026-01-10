cd84af051856652b7574a7e36646f87ba8b47882
sanchezJose07 <l18141223@queretaro.tecnm.mx>
2026-01-09 23:02:44 -0600

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -44,11 +48,13 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItems.title),
       subtitle: Text(menuItems.subtitle),
       onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const ButtonsScreen()
-            )
-        );
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen()
+        //     )
+        // );
+        // context.pushNamed(CardsScreen.name);
+        context.push(menuItems.link);
       },
     );
   }
